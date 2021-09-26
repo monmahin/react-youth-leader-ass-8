d87afa8a2571915093766da0e7c4d369b50a895e
monmahin <monmahin13@gmail.com>
2021-09-26 22:42:16 +0600

    leader commit

--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -10,14 +10,22 @@ const Cart = (props) => {
     }
     const title = cart.map(info => <div key={info.id} className="info" >
         <img src={info.img} alt="" />
-        <h3>{info.name}</h3>
+        <h3>{info.name}
+            <br />
+            <hr/>
+            <address
+             style={{ fontSize: '16px', textAlign: 'right', fontWeight:'200' }}>
+                {info.role}
+            </address>
+        </h3>
     </div>)
    
     return (
-        <div>
+        <div className="cart-container">
             <h1 className="add-leader"> {user} Added Leader: {cart.length}</h1>
-            <h2>{title}</h2>
+            <p>{title}</p>
             <hr/>
+        
             <h2 style={{color:'crimson'}}>Total cost: ${total}</h2>
             <hr/>
         </div>
